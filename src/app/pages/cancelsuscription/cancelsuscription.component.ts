8c4b68c28b8e2a90c625e772149a49735cf28b64
Cotefo <jantoniodn02@gmail.com>
2024-08-08 16:23:56 -0400

    update 2-08-24

--- src/app/pages/cancelsuscription/cancelsuscription.component.ts
+++ src/app/pages/cancelsuscription/cancelsuscription.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @Component({
   selector: 'app-cancelsuscription',
   standalone: true,
-  imports: [RouterLink ],
+  imports: [RouterLink, NzButtonModule, NzFormModule, RouterModule, ReactiveFormsModule, NzGridModule, FormsModule ],
   templateUrl: './cancelsuscription.component.html',
   styleUrl: './cancelsuscription.component.scss'
 })
--- src/app/pages/paymenthistory/paymenthistory.component.ts
+++ src/app/pages/paymenthistory/paymenthistory.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @Component({
   selector: 'app-paymenthistory',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NzButtonModule, NzFormModule, RouterModule, ReactiveFormsModule, NzGridModule, FormsModule ],
   templateUrl: './paymenthistory.component.html',
   styleUrl: './paymenthistory.component.scss'
 })
